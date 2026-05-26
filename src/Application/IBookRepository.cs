120b82eaae794c0d19d44b75ebeb20b61993431b
Sashen <SGovindasamy@dvtsoftware.com>
2026-05-26 09:12:19 +0200

    feat: add IBookRepository interface to Application layer

---
+++ src/Application/IBookRepository.cs
@@ -0,0 +1,11 @@
+namespace BookLibrary.Application;
+
+public interface IBookRepository
+{
+    Task<Guid> AddAsync(string title, string author, CancellationToken ct);
+    Task<List<BookDto>> GetAllAsync(CancellationToken ct);
+    Task<BookDto?> GetByIdAsync(Guid id, CancellationToken ct);
+    Task<List<BookDto>> SearchAsync(string term, CancellationToken ct);
+    Task<bool> UpdateAsync(Guid id, string title, string author, CancellationToken ct);
+    Task<bool> DeleteAsync(Guid id, CancellationToken ct);
+}
